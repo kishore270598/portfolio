97d683375cbed9af49d855f1a02bc06e7a3abe93
kishore270598 <kk94323@gmail.com>
2024-04-10 00:26:07 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import { Portwrapper } from './components/PortfolioWrapper';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Portwrapper/>
     </div>
   );
 }
---
+++ src/components/PortfolioWrapper.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const PortfolioWrapper = () => {
+  return (
+    <div className='PortfolioWrapper' >
+        <h1>about me</h1>
+    </div>
+  )
+}
+
